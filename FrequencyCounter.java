496cd7150cf2641bbe33e8e8098b3fb41f4d68e9
Hari Chandan Reddy <168167397+softwarechandhanreddy@users.noreply.github.com>
2025-09-18 23:02:03 +0530

    Arrays

---
+++ FrequencyCounter.java
@@ -0,0 +1,40 @@
+package com.codegnan.Arrays;
+
+
+import java.util.Scanner;
+
+public class FrequencyCounter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Array size :");
+		int n = sc.nextInt();
+        int[] arr = new int[n];
+        boolean[] p = new boolean[n];
+        System.out.println("Enter integers:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for(int i=0;i<n;i++) {
+        	if(p[i]) {
+        		continue;
+        	}
+	        int c = 1;
+	        for(int j= i+1;j<n;j++) {
+	        	if (arr[j] == arr[i]) {
+	        		c++;
+	        		p[j] = true;
+	        	}
+	        }
+	        System.out.println(arr[i]+" occurs " +c+ " Times");
+        }
+        for (int i = 0; i < n; i++) {
+            
+            System.out.println(arr[i]);
+        }
+
+	}
+}
+
+
